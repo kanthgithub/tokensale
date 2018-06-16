f11e981aee56b54b80480eebdeaed8e8245a8dd3
Jiaqian <jiaqian.me@qq.com>
2018-06-16 18:49:19 +0800

    Hide online config

--- GPX/truffle-config.js
+++ GPX/truffle-config.js
@@ -1,4 +1,21 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+    // See <http://truffleframework.com/docs/advanced/configuration>
+    // to customize your Truffle configuration!
+    
+    networks: {
+        "development": {
+            network_id: 2,
+            host: "localhost",
+            port: 8545
+        },
+        "live": {
+            network_id: 3,
+            host: '13.250.50.145',
+            port: 8545,
+            gas: 600000,
+            from: '0xfe767f199e679fe7b7615a7916d75e8b19eff3b3'
+        }
+    }
+
+
 };
--- GPX/truffle.js
+++ GPX/truffle.js
@@ -2,20 +2,4 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
 
-
-  /*
-  networks: {
-    "development": {
-      network_id: 2,
-      host: "localhost",
-      port: 8545
-    },
-    "live": {
-     network_id: 1,
-     host: 'https://api.myetherapi.com/eth',
-     port: 80
-   }
-  }
-  */
-
 };

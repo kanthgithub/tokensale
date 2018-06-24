71cda7ee282ebe4d1f54dceb26b0c116e40007b3
Jiaqian <jiaqian.me@qq.com>
2018-06-25 00:24:27 +0800

    Add comments

--- GPX/contracts/ParcelXGPX.sol
+++ GPX/contracts/ParcelXGPX.sol
@@ -157,14 +157,18 @@ contract ParcelXGPX is ERC20, MultiOwnable, Pausable, Convertible {
     }
 
     /**
-     * FEATURE 5): Convertible implements
+     * FEATURE 5): 'Convertible' implements
+     * Below actions would be performed after token being converted into mainchain:
+     * - Unsold tokens are discarded.
+     * - Tokens sold with bonus will be locked for a period (see Whitepaper).
+     * - Token distribution for team will be locked for a period (see Whitepaper).
      */
     function convertMainchainGPX(string destinationAccount, string extra) external returns (bool) {
         require(bytes(destinationAccount).length > 10 && bytes(destinationAccount).length < 128);
         require(balances[msg.sender] > 0);
         uint256 amount = balances[msg.sender];
         balances[msg.sender] = 0;
-        balances[tokenPool] = balances[tokenPool].add(amount);   // recycle ParcelX to tokenPool's init account
+        balances[tokenPool] = balances[tokenPool].add(amount);   // return GPX to tokenPool - the init account
         Converted(msg.sender, destinationAccount, amount, extra);
         return true;
     }
